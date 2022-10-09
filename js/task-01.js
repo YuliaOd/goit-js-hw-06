20292c6da023fa02307b0737e31670525a135236
YuliaOd <106181095+YuliaOd@users.noreply.github.com>
2022-10-09 18:53:47 +0300

    change 09.10

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,7 @@
-const categories = document.querySelectorAll('.item');
-console.log(' ');
-console.log(`Number of categories: ${categories.length}`);
-console.log(' ');
-categories.forEach(category => {
-    const group = [...category.children];
-    group.forEach(element => {
-        if (element.tagName === 'H2') {
-            console.log(`Elements: ${element.textContent}`);
-        }
-    });
-    group.forEach(element => {
-        if (element.tagName === 'UL') {
-            console.log(`Elements: ${element.children.length}`);
-        }
-    });
-    console.log(' ');
+const categoriesList = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesList.length}`)
+
+categoriesList.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
 });
